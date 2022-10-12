8a5e80b42a7fbf2d914cbecf3d9fddb7a519fda9
ILoveSlab <nunkeun0@naver.com>
2022-10-12 09:32:35 +0900

    test

---
+++ src/main/java/com/slab/slabweb/SlabwebApplication.java
@@ -0,0 +1,13 @@
+package com.slab.slabweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlabwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlabwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slab/slabweb/SlabwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slab.slabweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlabwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
